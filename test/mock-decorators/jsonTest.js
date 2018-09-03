d902adfdb30b3cf0ca5524db9c6badca7e69c610
xtx1130 <xtx1130@gmail.com>
2018-09-03 15:29:19 +0800

    lint: lint change

--- test/mock-decorators/jsonTest.js
+++ test/mock-decorators/jsonTest.js
@@ -1,20 +1,20 @@
-'use strict';
-require('./common/person');
+'use strict'
+require('./common/person')
 
 exports = module.exports = {
-	"id": "/jsontest",
-	"type": "object",
-	"properties": {
-		"test": {
-			"type": "boolean",
-			"required": "true"
-		},
-		"name": {
-			"type": "string",
-			"required": "true"
-		},
-		"params": {
-			"$ref": "/jsonTestParam"
-		}
-	}
+  'id': '/jsontest',
+  'type': 'object',
+  'properties': {
+    'test': {
+      'type': 'boolean',
+      'required': 'true'
+    },
+    'name': {
+      'type': 'string',
+      'required': 'true'
+    },
+    'params': {
+      '$ref': '/jsonTestParam'
+    }
+  }
 }
