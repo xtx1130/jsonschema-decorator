bda30d431de267586749ddc952acd7a91942b18e
xtx1130 <xtx1130@gmail.com>
2018-09-03 15:15:14 +0800

    pkg: change test base path

--- utils/scanner.js
+++ utils/scanner.js
@@ -6,11 +6,9 @@
 const fs = require('fs')
 const path = require('path')
 
-/* istanbul ignore next */
-let filePath = process.env.NODE_ENV === 'travis' ? '/test/mock-decorators/' : '/mock-decorators/'
-/* istanbul ignore next */
-let projectPath = process.env.NODE_ENV === 'travis' ? path.join(__dirname, '../') : process.cwd()
-let truePath = path.join(projectPath + filePath)
+let filePath = '/mock-decorators/'
+let projectPath = process.cwd()
+let truePath = path.join(projectPath, filePath)
 let str = []
 let _memoryPath = ''
 
